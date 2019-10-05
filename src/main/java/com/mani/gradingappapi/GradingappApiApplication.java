d8d6832bb0b7cf45677c294250305091b2f07708
Manikandan <manikandanbala002@gmail.com>
2019-10-05 11:38:55 +0530

    new spring

---
+++ src/main/java/com/mani/gradingappapi/GradingappApiApplication.java
@@ -0,0 +1,13 @@
+package com.mani.gradingappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradingappApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradingappApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mani/gradingappapi/GradingappApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mani.gradingappapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GradingappApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
